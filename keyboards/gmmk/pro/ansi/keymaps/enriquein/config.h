d3cf0396b4d100e51acd832d8c1ebdc444880a93
Enrique Ramirez <enriquein@gmail.com>
2021-11-02 18:59:02 -0400

    Try putting numpad numbers in the top row, in a layer on top of the regular number keys.

--- keyboards/gmmk/pro/ansi/keymaps/enriquein/config.h
+++ keyboards/gmmk/pro/ansi/keymaps/enriquein/config.h
@@ -1,11 +1,5 @@
 #pragma once
 
-#ifdef TAPPING_TOGGLE
-#undef TAPPING_TOGGLE
-#endif
-
-#define TAPPING_TOGGLE 2
-
 #ifndef ENCODER_ENABLE
 #define ENCODER_ENABLE
 #endif
--- keyboards/gmmk/pro/ansi/keymaps/enriquein/keymap.c
+++ keyboards/gmmk/pro/ansi/keymaps/enriquein/keymap.c
@@ -46,16 +46,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,          KC_END,
         KC_LOCK, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,           KC_PGUP,
         KC_LSFT,          KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,          KC_RSFT, KC_UP,   KC_PGDN,
-        KC_LCTL, KC_LGUI, KC_LALT,                            KC_SPC,                             KC_RALT, TT(1),   KC_RCTL, KC_LEFT, KC_DOWN, KC_RGHT
+        KC_LCTL, KC_LGUI, KC_LALT,                            KC_SPC,                             KC_RALT, MO(1),   KC_RCTL, KC_LEFT, KC_DOWN, KC_RGHT
     ),
 
     [1] = LAYOUT(
         RESET  , KC_MYCM, _______, _______, _______, KC_MPRV, KC_MNXT, KC_MPLY, KC_MSTP, _______, _______, _______, _______, _______,          KC_NLCK,
-        _______, RGB_TOG, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_DEL,          _______,
-        _______, _______, RGB_VAI, _______, _______, _______, _______, _______, _______, KC_P7  , KC_P8  , KC_P9  , _______, _______,            _______,
-        _______, _______, RGB_VAD, _______, _______, _______, _______, _______, _______, KC_P4  , KC_P5  , KC_P6  ,          _______,          _______,
-        _______,          _______, RGB_HUI, _______, _______, _______, NK_TOGG, _______, KC_P1  , KC_P2  , KC_P3  ,          _______, RGB_MOD, _______,
-        _______, _______, _______,                            _______,                            KC_P0  , _______, _______, RGB_SPD, RGB_RMOD, RGB_SPI
+        _______, KC_P1  , KC_P2  , KC_P3  , KC_P4  , KC_P5  , KC_P6  , KC_P7  , KC_P8  , KC_P9  , KC_P0  , _______, _______, KC_DEL,           _______,
+        _______, RGB_TOG, RGB_VAI, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______,
+        _______, _______, RGB_VAD, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______,          _______,
+        _______,          _______, RGB_HUI, _______, _______, _______, NK_TOGG, _______, _______, _______, _______,          _______, RGB_MOD, _______,
+        _______, _______, _______,                            _______,                            _______, _______, _______, RGB_SPD, RGB_RMOD, RGB_SPI
     ),
 
 
