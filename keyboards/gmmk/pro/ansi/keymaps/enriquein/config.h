39142659624fe5abb59c3d1661f05e10f8cfb85c
Enrique Ramirez <enriquein@gmail.com>
2022-04-03 11:51:50 -0400

    Fix problem where LEDs stay on after system shutdown.

--- keyboards/gmmk/pro/ansi/keymaps/enriquein/config.h
+++ keyboards/gmmk/pro/ansi/keymaps/enriquein/config.h
@@ -6,3 +6,6 @@
 
 /* Force NKRO on boot up regardless of the setting saved in the EEPROM (uncomment to enable it) */
 #define FORCE_NKRO
+
+// Need to define this so that the leds turn off when USB is off or suspended.
+#define RGB_DISABLE_WHEN_USB_SUSPENDED
