89959ea921aa0672bdee8a3ea6e5000238b67217
Enrique Ramirez <enriquein@gmail.com>
2022-04-15 19:35:07 -0400

    Added timeout to turn off leds when inactive.

--- keyboards/gmmk/pro/ansi/keymaps/enriquein/config.h
+++ keyboards/gmmk/pro/ansi/keymaps/enriquein/config.h
@@ -9,3 +9,6 @@
 
 // Need to define this so that the leds turn off when USB is off or suspended.
 #define RGB_DISABLE_WHEN_USB_SUSPENDED
+
+// Turn RGB off after N milliseconds. Covers when power goes out and comes back on. This will usually turn on the LEDs even though the PC is off.
+#define RGB_DISABLE_TIMEOUT 1800000
