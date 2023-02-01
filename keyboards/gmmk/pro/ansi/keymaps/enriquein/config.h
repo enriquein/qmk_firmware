b8d5d4c691f23fab585e1b431efcfeb2238d7164
Enrique Ramirez <enriquein@gmail.com>
2023-02-01 11:20:50 -0400

    Raise debounce to 8ms. Redefine RGB_DISABLE_TIMEOUT to see if it works more consistently.

--- keyboards/gmmk/pro/ansi/keymaps/enriquein/config.h
+++ keyboards/gmmk/pro/ansi/keymaps/enriquein/config.h
@@ -11,4 +11,13 @@
 #define RGB_DISABLE_WHEN_USB_SUSPENDED
 
 // Turn RGB off after N milliseconds. Covers when power goes out and comes back on. This will usually turn on the LEDs even though the PC is off.
+#ifdef RGB_DISABLE_TIMEOUT
+#undef RGB_DISABLE_TIMEOUT
+#endif
 #define RGB_DISABLE_TIMEOUT 1800000
+
+// Raise it to 8ms to see if it helps
+#ifdef DEBOUNCE
+#undef DEBOUNCE
+#endif
+#define DEBOUNCE 8
